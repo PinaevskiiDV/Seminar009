bca323add0ac8b6ab3327a9ad35ec92b790ece37
Dmitrii <pinochet37@mail.ru>
2022-10-04 19:16:25 +0300

    fixed Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите N");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(PrintNamber(num, 1));
+
+string PrintNamber(int start, int end)
+{
+    if (start == end)
+    {
+        return start.ToString();
+    }
+    return (start + "," + PrintNamber(start - 1, end));
+}
