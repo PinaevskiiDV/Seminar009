bc09d3fc4ee8f1d9ccc4df66774f05509b722cf6
Dmitrii <pinochet37@mail.ru>
2022-10-04 19:33:22 +0300

    added Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите M");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите N");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(PrintNamber(num1, num2));
+
+int PrintNamber(int start, int end)
+{
+    if (start == end)
+    {
+        return start;
+    }
+    return (start + PrintNamber(start + 1, end));
+}
